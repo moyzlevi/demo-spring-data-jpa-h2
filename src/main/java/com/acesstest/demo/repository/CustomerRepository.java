19993e7a49829f7920c55d0cf00fd14612d2efd1
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-18 14:58:38 -0300

    feat:created project with h2 and JPA

---
+++ src/main/java/com/acesstest/demo/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.acesstest.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.acesstest.demo.model.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	
+	List<Customer> findByLastName(String lastName);
+	
+	Customer findById(long id);
+	
+}
---
+++ src/test/java/com/acesstest/demo/AccessTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acesstest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
